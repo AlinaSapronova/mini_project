1e2c423c2a7cb6235fa9da28044716922ff5d7fa
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-07 19:36:13 +0000

    created api request

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'net/http'
+require 'json'
+
+
+
+
+
+def getRecipe
+  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=26b3ada9&app_key=%2036259b904524c4e987b5a093707654fa")
+  uri = URI(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(url) 
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+  results = response_obj.fetch("hits")
+  return results
+end
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  @recipe = getRecipe()
+
+  erb(:home)
 end
