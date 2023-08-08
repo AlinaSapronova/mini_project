5470d26936774f01873abefe756eccb79e204c61
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-08 17:49:50 +0000

    post req fixed

--- app.rb
+++ app.rb
@@ -21,11 +21,24 @@ end
 
 
 get("/") do
-
+@recipe = getRecipe()
   erb(:home)
 end
 
-get("/search") do
-  @recipe = getRecipe()
+post("/recipe_search"){
+
+  search_term = params.fetch("search_term")
+
+  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{search_term}&app_id=26b3ada9&app_key=%2036259b904524c4e987b5a093707654fa")
+  uri = URI(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(url) 
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+  results = response_obj.fetch("hits")
+  @recipe = results
+
   erb(:recipe)
-end
+
+}
