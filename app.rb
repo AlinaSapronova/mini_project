ce8951dff28fd7ddb5c1fba755040becd25ddcde
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-10 03:04:56 +0000

    everything working fine, ready for next step

--- app.rb
+++ app.rb
@@ -7,16 +7,11 @@ require 'json'
 
 
 
-def getRecipe
-  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=avocado&app_id=26b3ada9&app_key=%2036259b904524c4e987b5a093707654fa")
-  uri = URI(url)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(url) 
-  response = Net::HTTP.get(uri)
+def getRecipe(search_term)
+  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{search_term}&app_id=26b3ada9&app_key=%2036259b904524c4e987b5a093707654fa")
+  response = Net::HTTP.get(url)
   response_obj = JSON.parse(response)
   results = response_obj.fetch("hits")
-  return results
 end
 
 
@@ -25,18 +20,10 @@ get("/") do
   erb(:home)
 end
 
-post("/recipe_search"){
+post("/recipe_search") do
 
   search_term = params.fetch("search_term")
-
-  url = URI("https://api.edamam.com/api/recipes/v2?type=public&q=#{search_term}&app_id=26b3ada9&app_key=%2036259b904524c4e987b5a093707654fa")
-  uri = URI(url)
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  request = Net::HTTP::Get.new(url) 
-  response = Net::HTTP.get(uri)
-  response_obj = JSON.parse(response)
-  @results = response_obj.fetch("hits")
+  @results = getRecipe(search_term)
 
   erb(:recipe)
-}
+end
